654c9fe3186b193e3be69f1c3cc5547cf7a983fd
asnjduarte <124079530+asnjduarte@users.noreply.github.com>
2023-03-03 19:27:29 -0600

    Update BasicPrivacyPolicy.php
    added the section to create basic policy page and add the basic privacy policy

--- PolicyTemplates/BasicPrivacyPolicy/BasicPrivacyPolicy.php
+++ PolicyTemplates/BasicPrivacyPolicy/BasicPrivacyPolicy.php
@@ -1,5 +1,8 @@
 <?php
 
+$page_slug = "politica-de-privacidad-basico";
+$page_name = "Politica de privacidad";
+
 $basic_privacy_policy_data = '
 <?php 
 $policyId = 1; //basic privacy policy
@@ -12,6 +15,20 @@ fwrite($basic_privacy_policy, $basic_privacy_policy_data);
 
 $basic_privacy_function_data = '
 <?php //generic policy view
+$page_slug = \''. $page_slug .'\'; // Slug of the Post
+$new_page = array(
+    \'post_type\'     => \'page\', 				// Post Type Slug eg: \'page\', \'post\'
+    \'post_title\'    => \''.$page_name.'\',	// Title of the Content
+    \'post_content\'  => \'[display_basic_privacy_policy_view]\',	// Content
+    \'post_status\'   => \'publish\',			// Post Status
+    \'post_author\'   => 1,					// Post Author ID
+    \'post_name\'     => '.$page_slug.'			// Slug of the Post
+);
+
+if (!get_page_by_path( '.$page_slug.', OBJECT, \'page\')) { // Check If Page Not Exits
+    $new_page_id = wp_insert_post($new_page);
+}
+
 function fetch_bppv(){add_shortcode(\'display_basic_privacy_policy_view\', \'fetch_basic_privacy_policy_view\');} 
 add_action(\'init\', \'fetch_bppv\');
 function fetch_basic_privacy_policy_view() {
