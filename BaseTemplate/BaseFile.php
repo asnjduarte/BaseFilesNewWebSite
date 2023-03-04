e08fcadeca834448d2af8b3e97c43e31ce752e33
asnjduarte <124079530+asnjduarte@users.noreply.github.com>
2023-03-03 20:12:50 -0600

    Update BaseFile.php
    added the scroll animation

--- BaseTemplate/BaseFile.php
+++ BaseTemplate/BaseFile.php
@@ -564,6 +564,7 @@ $(document).ready(function(){
  
  });
 
+/* sanitize with jquery */
  function txtOnly() {
     //var regExp = 
     var string = $(this).val().replace(/[^a-zA-Z ]/g,""); 
@@ -588,6 +589,48 @@ $(document).ready(function(){
     $(this).val(string);
    }
    $(\'.emailChar\').keyup(emailChar);
+   /* end sanitize with jquery */
+   
+   /*Scroll animation*/
+(function(){
+    // Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+         function(callback){ window.setTimeout(callback, 1000/60)};
+   var elementsToShow = document.querySelectorAll('.sos');
+   function loop() {
+ elementsToShow.forEach(function (element) {
+   if (isElementInViewport(element)) {
+     element.classList.add('is-visible');
+   } else {
+     element.classList.remove('is-visible');
+   }
+ });
+
+ scroll(loop);
+}
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+ // special bonus for those using jQuery
+ if (typeof jQuery === "function" && el instanceof jQuery) {
+   el = el[0];
+ }
+ var rect = el.getBoundingClientRect();
+ return (
+   (rect.top <= 0
+     && rect.bottom >= 0)
+   ||
+   (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+     rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+   ||
+   (rect.top >= 0 &&
+     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+ );
+}
+}());
+/*End Scroll animation*/
 ';
 $splash_js = fopen(__DIR__.'\js\footerBundle.js', "w");
 fwrite($splash_js, $splash_js_data);
